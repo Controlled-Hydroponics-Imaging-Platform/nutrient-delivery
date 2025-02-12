5ab03cd80ccb06fe2cab97a092f523aba4fb5343
moha7108 <moha7108@protonmail.com>
2025-02-11 20:26:03 -0500

    declare missing override functions in the Ultrasonic header files that were already declared in the implementation file

--- nutrino/src/Ultrasonic_A02YYUW.h
+++ nutrino/src/Ultrasonic_A02YYUW.h
@@ -9,6 +9,8 @@
 class ultrasonic_A0266UW : public Sensor{
     public:
         ultrasonic_A0266UW(int rx, int tx, int delay_ms=100);
+        void readSensor() override;
+        void begin(int baud_rate) override;
 
     private:
         int rx_dpin_;
