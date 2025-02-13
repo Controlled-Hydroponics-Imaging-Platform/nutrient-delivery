438824bab326fd1f48eb6f07a658b91a58993ff2
moha7108 <moha7108@protonmail.com>
2025-02-13 16:40:40 -0500

    add DS18B20 Sensor package, header and implementation files, now need to test before merging  to main

--- nutrino/nutrino.ino
+++ nutrino/nutrino.ino
@@ -1,17 +1,26 @@
 #include "src/Ultrasonic_A02YYUW.h"
+#include "src/Temp_DS18B20.h"
+#include "lib/OneWire/OneWire.h"
 
+#define ONE_WIRE_BUS_PIN 10
+OneWire one_wire_bus(ONE_WIRE_BUS_PIN);
+
+Sensor* DS18B20 = new Temp_DS18B20(one_wire_bus);
 Sensor* ultrasonic = new Ultrasonic_A02YYUW(4, 3);
-float distance;
+float distance, water_temp;
 
 void setup(){
     Serial.begin(9600);
-    ultrasonic->begin(9600);
+    ultrasonic->begin(9600); // uses software serial so needs baudrate
+    DS18B20->begin();
 }
 
 
 void loop(){
 
     distance = ultrasonic->readSensor();
+    water_temp = DS18B20->readSensor();
+
 
     Serial.println(distance);
 
---
+++ nutrino/src/Temp_DS18B20.cpp
@@ -0,0 +1,31 @@
+#include "Temp_DS18B20.h"
+#include <math.h>
+
+Temp_DS18B20::Temp_DS18B20 (const OneWire& ow_bus, int delay_ms) : oneWire_bus_(ow_bus), DS18B20_(&ow_bus){
+    setDelay(delay_ms);
+}
+
+float Temp_DS18B20::readSensor(){
+// Returns temperature in C
+
+  DS18B20_.requestTemperatures();  // Send command to get temperature readings
+
+  temperature_ = DS18B20_.getTempCByIndex(0);  // Read temperature in Celsius
+
+  // Check if the sensor is connected
+  if (temperature_ == DEVICE_DISCONNECTED_C) {
+    Serial.println("Error Reading value from Temperature_DS18B20: DS18B20 not detected!");
+  }else{
+    return temperature_;
+  }
+
+  return NAN;
+}
+
+void Temp_DS18B20::begin(int baud_rate){
+    this->begin();
+}
+
+void Temp_DS18B20::begin(){
+    DS18B20_.begin();
+}
